47cc9415b587f2f7998f7923d949d1071b8f3e83
Rene Kaesler <renekaesler@gmail.com>
2025-05-28 22:35:31 +0200

    add basic meta tags to the blog

---
+++ config.ts
@@ -0,0 +1,6 @@
+export const SITE = {
+  title: "kaesler.dev",
+  description: "Rene Kaesler’s corner for sharing know-how and thoughts on coding and IT.",
+  author: "Rene Kaesler",
+  lang: "en",
+} as const;
